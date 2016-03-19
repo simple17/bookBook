f936baef3254b2f3362cddc54263934f67aaff3a
dMitin <denis.mitin@firstlinesoftware.com>
2016-03-19 16:37:17 +0300

    Сделал поиск, но возвращает все данные

--- server/src/main/java/bookBook/neo4j/Neo4jApi.java
+++ server/src/main/java/bookBook/neo4j/Neo4jApi.java
@@ -24,12 +24,14 @@ public class Neo4jApi extends AbstractVerticle {
 		 * Сохранение сотрудника в neo4j
 		 */
 		String addBookQuery = "CREATE (b:Book { props } ) return id(b)";
+        String searchBookQuery = "MATCH (n:Book) RETURN n";
 		JsonObject queryTemplate = new JsonObject()
-				 //.put("query", addBookQuery)
-                .put("query", "")
                 .put("params", new JsonObject());
 
 
+        /**
+         * Добавление книги
+         */
 		eb.consumer("neo4j.book.addBook", addBookMessage -> {
 
 			JsonObject bookData = (JsonObject) addBookMessage.body();
@@ -51,6 +53,31 @@ public class Neo4jApi extends AbstractVerticle {
 				 }
 			});
 		});
+
+        eb.consumer("neo4j.book.searchBook", searchBookMessage -> {
+
+            JsonObject bookData = (JsonObject) searchBookMessage.body();
+            JsonObject req = new JsonObject(queryTemplate.toString());
+            //req.getJsonObject("params").put("props", bookData);
+            req.put("query", searchBookQuery);
+
+            eb.send("neo4j.runCypher", req, cypherResponse -> {
+
+                if (cypherResponse.succeeded()) {
+                    // удачно сохранили в neo4j, надо достать и отправить id
+                    JsonObject neo4jResponseData = (JsonObject) cypherResponse.result().body();
+                    /*
+                    Integer id = neo4jResponseData.getJsonArray("data").getJsonArray(0).getInteger(0);
+                    JsonObject eventResponse = new JsonObject().put("id", id);
+                    */
+                    String resp = neo4jResponseData.toString();
+                    searchBookMessage.reply(resp);
+                } else {
+                    // сохранение с ошибкой, отправлям fail
+                    searchBookMessage.fail(0, cypherResponse.cause().getMessage());
+                }
+            });
+        });
 
 
 
