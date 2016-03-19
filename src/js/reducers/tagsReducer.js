d8e3848135d10835085c45dfe9730fc38d56bcf1
Ulyana Morozova <Ulyana.Morozova@firstlinesoftware.com>
2016-03-20 01:41:57 +0300

    create new tag

--- src/js/reducers/tagsReducer.js
+++ src/js/reducers/tagsReducer.js
@@ -1,15 +1,15 @@
 export default (state = [], action) => {
   switch (action.type) {
     case 'UPDATE_TAGS':
-      return action.tags.map(t => {
-        return {
-          id: t.id,
-          name: t.data.name
-        }
-      })
+      return action.tags
       break;
     case 'CLEAR_TAGS':
       return [];
+    case 'ADD_TAG':
+      return [...state, {
+        id: action.id,
+        name: action.name
+      }];
     default:
       return state;
   }
