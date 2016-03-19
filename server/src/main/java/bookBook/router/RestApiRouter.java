67e3d57535aadf9eb15c455a729d8e87666197f5
dMitin <denis.mitin@firstlinesoftware.com>
2016-03-19 14:20:01 +0300

    1. добавляю BookRouter

--- server/src/main/java/bookBook/router/RestApiRouter.java
+++ server/src/main/java/bookBook/router/RestApiRouter.java
@@ -1,5 +1,6 @@
 package bookBook.router;
 
+import bookBook.router.book.BookRouter;
 import io.vertx.core.Vertx;
 import io.vertx.core.file.FileSystem;
 import io.vertx.core.http.HttpMethod;
@@ -7,9 +8,6 @@ import io.vertx.ext.web.Router;
 
 import java.util.HashMap;
 import java.util.Map;
-
-import static bookBook.Util.println;
-
 
 
 /**
@@ -19,36 +17,9 @@ public class RestApiRouter {
 
     public static void initRestRouter(Router router, Vertx vertx){
 
+        Router bookAPI = Router.router(vertx);
+        BookRouter.initBookRouter(bookAPI, vertx);
+        router.mountSubRouter("/book", bookAPI);
 
-        FileSystem fs = vertx.fileSystem();
-        String searchResponse = "../doc/4.8-bookSearch-response.json";
-        Map<String,String> responses = new HashMap<>();
-
-
-        fs.readFile(searchResponse, res-> {
-            if (res.succeeded()) {
-                System.out.println(searchResponse + " read");
-                responses.put(searchResponse, res.result().toString());
-                System.out.println(responses.get(searchResponse));
-            } else {
-                System.out.println(searchResponse + " read error");
-            }
-        });
-
-
-        router.route()
-                .path("/book/search")
-                .method(HttpMethod.GET).handler(rc -> {
-
-            /*
-            rc.response().putHeader("Content-Type", "application/json");
-            String id = rc.request().getParam("id");
-            eb.send("es.getUserById", id, neo4jResponse -> {
-                rc.response().putHeader("Content-type", "application/json; charset=utf-8");
-                rc.response().end(neo4jResponse.result().body().toString());
-            });
-            */
-            rc.response().end(responses.get(searchResponse));
-        });
     }
 }
--- server/src/main/java/bookBook/router/book/BookRouter.java
+++ server/src/main/java/bookBook/router/book/BookRouter.java
@@ -1,13 +1,49 @@
 package bookBook.router.book;
 
 import io.vertx.core.Vertx;
+import io.vertx.core.file.FileSystem;
+import io.vertx.core.http.HttpMethod;
 import io.vertx.ext.web.Router;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by DMitin on 19.03.2016.
  */
 public class BookRouter {
     public static void initBookRouter(Router router, Vertx vertx){
+
+        FileSystem fs = vertx.fileSystem();
+        String searchResponse = "../doc/4.8-bookSearch-response.json";
+        Map<String,String> responses = new HashMap<>();
+
+
+        fs.readFile(searchResponse, res-> {
+            if (res.succeeded()) {
+                System.out.println(searchResponse + " read");
+                responses.put(searchResponse, res.result().toString());
+                System.out.println(responses.get(searchResponse));
+            } else {
+                System.out.println(searchResponse + " read error");
+            }
+        });
+
+
+        router.route()
+                .path("/search")
+                .method(HttpMethod.POST).handler(rc -> {
+
+            /*
+            rc.response().putHeader("Content-Type", "application/json");
+            String id = rc.request().getParam("id");
+            eb.send("es.getUserById", id, neo4jResponse -> {
+                rc.response().putHeader("Content-type", "application/json; charset=utf-8");
+                rc.response().end(neo4jResponse.result().body().toString());
+            });
+            */
+            rc.response().end(responses.get(searchResponse));
+        });
 
     }
 }
