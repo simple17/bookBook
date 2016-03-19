10d18ab91a3a3eebcab66b24a066c925b9b54dcc
dMitin <denis.mitin@firstlinesoftware.com>
2016-03-19 21:47:53 +0300

    Сделал получение новых тэгов

--- server/src/main/java/bookBook/router/tag/TagRouter.java
+++ server/src/main/java/bookBook/router/tag/TagRouter.java
@@ -1,6 +1,7 @@
 package bookBook.router.tag;
 
 import io.vertx.core.Vertx;
+import io.vertx.core.eventbus.EventBus;
 import io.vertx.core.http.HttpMethod;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
@@ -11,26 +12,28 @@ import io.vertx.ext.web.Router;
 public class TagRouter {
     public static void initTagRouter(Router router, Vertx vertx){
 
+        EventBus eb = vertx.eventBus();
+
         router.route()
                 .path("/all")
                 .method(HttpMethod.GET).handler(rc -> {
 
             System.out.println("/tag/all");
 
-            /*
-            eb.send("neo4j.book.searchBook", new JsonObject(), neo4jResponse -> {
+
+            eb.send("neo4j.book.getAllTags", new JsonObject(), neo4jResponse -> {
                 rc.response().putHeader("Content-type", "application/json; charset=utf-8");
                 rc.response().putHeader("Access-Control-Allow-Origin", "*");
                 rc.response().end(neo4jResponse.result().body().toString());
             });
-            */
+
 
             /*
             rc.response().putHeader("Access-Control-Allow-Origin", "*");
             rc.response().end(responses.get(searchResponse));
             */
 
-            rc.response().end("ok");
+            //rc.response().end("ok");
         });
 
     }
