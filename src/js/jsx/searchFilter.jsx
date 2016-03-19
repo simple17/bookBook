2b5d90e72d3e9b55b35feb978e7acd72f5526aa2
Ulyana Morozova <Ulyana.Morozova@firstlinesoftware.com>
2016-03-19 16:42:45 +0300

    restyling of search filter

--- src/js/jsx/searchFilter.jsx
+++ src/js/jsx/searchFilter.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 export default ({loadMore}) => {
   return (
-    <div class="form-group">
-      <input type="text" className="form-control" id="txtSearch"/>
-      <span class="glyphicon glyphicon-name"></span>
-      <button onClick={loadMore}>Load books</button>
+  <div class="form-group">
+    <div className="col-md-11">
+      <input type="text" className="form-control" id="txtSearch" />
     </div>
+    <span onClick={loadMore} className="col-md-1 glyphicon glyphicon-search"></span>
+  </div>
   );
 };
